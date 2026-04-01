b4ed8b715371776f8ebea97300ef4e0ec04bc1dd
Kath <kathssic@gmail.com>
2026-04-01 14:13:04 -0500

    Ajuste menu

--- assets/scripts/respMenu.js
+++ assets/scripts/respMenu.js
@@ -3,20 +3,51 @@ const navbar = document.getElementById('navcont')
 const menuItems = document.querySelector('.menuItems')
 const bckgMenuList = document.querySelector('.bckgNavList')
 
-function openRespMenu(){
-    navbar.classList.add('showMenu')
-    openButton.setAttribute('aria-expanded','true')
-    navbar.removeAttribute('inert')
-    menuItems.classList.add('active')
-    bckgMenuList.classList.add('active')
+
+const media = window.matchMedia("(width < 768px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e) {
+    const isMobile = e.matches
+    console.log(isMobile)
+    if (isMobile) {
+        navbar.setAttribute('inert', '')
+    }
+    else {
+        navbar.removeAttribute('inert')
+        navbar.classList.remove('showMenu')
+        navbar.classList.remove('menuAnimation')
+        menuItems.classList.remove('active')
+        bckgMenuList.classList.remove('active')
+    }
 }
 
-function closeRespMenu(){
-    navbar.classList.remove('showMenu')
-    openButton.setAttribute('aria-expanded','false')
-    navbar.setAttribute('inert','')
+function openRespMenu() {
+    navbar.classList.add('menuAnimation')
+    navbar.removeAttribute('inert')
 
-    menuItems.classList.remove('active')
+    void navbar.offsetWidth;
+
+    navbar.classList.add('showMenu')
+    menuItems.classList.add('active')
+    bckgMenuList.classList.add('active')
+    openButton.setAttribute('aria-expanded', 'true')
+}
+
+function closeRespMenu() {
+    navbar.classList.remove('showMenu')
     menuItems.classList.remove('active')
     bckgMenuList.classList.remove('active')
-}   
+    openButton.setAttribute('aria-expanded', 'false')
+
+
+    setTimeout(() => {
+        navbar.classList.remove('menuAnimation')
+        navbar.setAttribute('inert', '')
+    }, 250)
+}
+
+
+
+updateNavbar(media)
